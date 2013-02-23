6b7c484f64dd88c7fd78ad2844d94d175ae050a4
Mark Duffy <markaduffy@gmail.com>
2013-02-23 13:42:07 -0500

    app controller

--- app_controller.php
+++ app_controller.php
@@ -32,7 +32,7 @@
  */
 class AppController extends Controller {
 
-	var $helpers = array('Html','Form','Session','Js');
+	var $helpers = array('Html','Form','Session','Js','Javascript');
 	var $components = array(
 		'Session',
 		'RequestHandler',
